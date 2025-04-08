50b66569233f04ce84eb534c9d2f749b70b0ac14
danielpm18 <dperezmellam@gmail.com>
2025-04-08 13:01:40 +0200

    Creación de constructores y funciones. 08/04/2025

--- proyecto/src/model/Producto.java
+++ proyecto/src/model/Producto.java
@@ -2,11 +2,17 @@ package model;
 
 public class Producto {
 
+    public String nombre;
     public String alergeno;
+    public int precio;
+    public int cantidad;
 
-    public Producto (String alergeno) {
+    public Producto (String nombre, String alergeno, int precio, int cantidad) {
 
+        this.nombre = nombre;
         this.alergeno = alergeno;
+        this.precio = precio;
+        this.cantidad = cantidad;
 
     }
 
